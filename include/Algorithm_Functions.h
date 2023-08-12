d36c2a9963137b91c4e95b8d2210a69ec42d8c65
Dilesh Vallabh <94227701+DileshVallabh@users.noreply.github.com>
2023-08-12 15:54:12 +0200

    Move header files to standalone file for factorisation algorithms

--- include/Algorithm_Functions.h
+++ include/Algorithm_Functions.h
@@ -27,35 +27,6 @@ std::vector<T> eratosthenes(T N_limit);
 
 }  // namespace Sieves
 
-/**
- * @brief Algorithms to find the factors of a number.
- */
-namespace Factorisation {
-
-/**
- * @brief Find all the prime factors of a number.
- *
- * @tparam T
- * @param N Number to factorise.
- * @return std::vector<T> A vector containing the prime factors.
- */
-template <typename T>
-std::vector<T> prime_factorise(T N);
-
-/**
- * @brief Compute the exponents for each prime factor from an explicit list of
- * primes.
- *
- * @tparam T
- * @param prime_factors List of all prime factors.
- * @return std::vector<std::pair<T, T>> A vector containing a pair of the form
- * (prime, exponent).
- */
-template <typename T>
-std::vector<std::pair<T, T>> prime_exponents(std::vector<T>& prime_factors);
-
-}  // namespace Factorisation
-
 namespace Logarithmic_Integral {
 
 /**
---
+++ include/Factorisation.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+
+/**
+ * @brief Algorithms to find the factors of a number.
+ */
+namespace Factorisation {
+
+/**
+ * @brief Find all the prime factors of a number.
+ *
+ * @tparam T
+ * @param N Number to factorise.
+ * @return std::vector<T> A vector containing the prime factors.
+ */
+template <typename T>
+std::vector<T> prime_factorise(T N);
+
+/**
+ * @brief Compute the exponents for each prime factor from an explicit list of
+ * primes.
+ *
+ * @tparam T
+ * @param prime_factors List of all prime factors.
+ * @return std::vector<std::pair<T, T>> A vector containing a pair of the form
+ * (prime, exponent).
+ */
+template <typename T>
+std::vector<std::pair<T, T>> prime_exponents(std::vector<T>& prime_factors);
+
+}  // namespace Factorisation
+
+#include <Factorisation.tcc>
